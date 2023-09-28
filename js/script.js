3251c641d93cb72fe92345ba81eeb9a899284353
ujjwalsingh0312 <92951690+ujjwalsingh0312@users.noreply.github.com>
2023-09-28 09:19:39 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -42,3 +42,20 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 };
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzbF8O4gQjlcX3flzyeqvcsMTMwMKD_UL-OUP1ZijG7E8sCl6f6D1nUbvmPiBHx-BVt-g/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+            form.reset();
+            msg.innerHTML = "Message sent Successfully, \n Thanks for Contacting!";
+            setTimeout(function(){
+                msg.innerHTML = "";
+            },5000);
+        })
+        .catch(error => console.error('Error!', error.message))
+});
